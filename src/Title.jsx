407642444f1ffe2c9f7fd305f3b5efcf0dc1c3db
mirulhg <mirulhg@gmail.com>
2022-12-23 02:36:45 +0700

    belajar membuat todo dengan menggunakan react

---
+++ src/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h2>REACT TO DO LIST</h2>
+    </div>
+  );
+};
+
+export default Title;
